6cfbbb68270eaab4804f32c430e61b310e3f3ea9
Nabil Fannane <nabil@goadroit.com>
2020-07-17 21:33:41 -0700

    Updates

---
+++ precache-manifest.77e7e4e0744b67d300d8a579aa09a184.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c64dae2715953b8a8c94461d168db34",
+    "url": "/ImageFinder/index.html"
+  },
+  {
+    "revision": "02eeb1cacc84d84d5244",
+    "url": "/ImageFinder/static/css/main.050523cc.chunk.css"
+  },
+  {
+    "revision": "d1b32696e9bacf0326a9",
+    "url": "/ImageFinder/static/js/2.ab6202fe.chunk.js"
+  },
+  {
+    "revision": "971b4e562d3abe5fe0995509ffca7edb",
+    "url": "/ImageFinder/static/js/2.ab6202fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02eeb1cacc84d84d5244",
+    "url": "/ImageFinder/static/js/main.640c8bc7.chunk.js"
+  },
+  {
+    "revision": "45b84a79ac537dfff328",
+    "url": "/ImageFinder/static/js/runtime-main.63868890.js"
+  }
+]);
